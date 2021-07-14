35d8646c2444f1e08f9925450edfffd236ae42e5
Bluette1 <marylene.sawyer@gmail.com>
2021-07-14 09:01:07 +0200

    Display users from the API

--- screens/ContactsScreen.tsx
+++ screens/ContactsScreen.tsx
@@ -1,11 +1,23 @@
-import * as React from 'react';
+import React, {useEffect, useState} from 'react';
 import { StyleSheet, FlatList } from 'react-native';
 import { Text, View } from '../components/Themed';
-import users from '../data/Users'
 import ContactListItem from '../components/ContactListItem'
+import { graphqlOperation, API } from 'aws-amplify';
+import {listUsers} from '../src/graphql/queries';
 
 export default function ContactsScreen() {
-
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const userData = await API.graphql(graphqlOperation(listUsers));
+        setUsers(userData.data.listUsers.items);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    fetchUser();
+  }, []);
   return (
     <View style={styles.container}>
       <FlatList 
