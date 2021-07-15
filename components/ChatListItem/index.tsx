ce37b13831a39c6a0ac0823aec7d6e17a485a68f
Bluette1 <marylene.sawyer@gmail.com>
2021-07-15 04:50:52 +0200

    Implement the chat screen

--- components/ChatListItem/index.tsx
+++ components/ChatListItem/index.tsx
@@ -1,35 +1,51 @@
 import moment from 'moment';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Auth } from 'aws-amplify';
 import { View, Text, Image, TouchableWithoutFeedback } from 'react-native';
 import { ChatRoom } from '../../types';
-import styles from './style'
-import {useNavigation} from '@react-navigation/native'
+import styles from './style';
+import {useNavigation} from '@react-navigation/native';
 
 export type ChatListItemProps = {
   chatRoom: ChatRoom;
 }
 
 const ChatListItem = (props: ChatListItemProps) => {
+  const [otherUser, setOtherUser] = useState(null);
   const { chatRoom } = props;
 
   const navigation = useNavigation();
-
-  const user = chatRoom.users[1];
   const onClick = () => {
-    navigation.navigate('ChatRoom', {id: chatRoom.id, name: user.name})
+    navigation.navigate('ChatRoom', {id: chatRoom.id, name: otherUser.name});
+  }
+  useEffect(() => {
+    const getOtherUser = async () => {
+      const userInfo = await Auth.currentAuthenticatedUser({ bypassCache: true });
+
+    if (chatRoom.chatRoomUsers.items[0].user.id === userInfo.attributes.sub) {
+      setOtherUser(chatRoom.chatRoomUsers.items[1].user);
+    } else {
+      setOtherUser(chatRoom.chatRoomUsers.items[0].user);
+    }
+    };
+    getOtherUser();
+  }, []);
+
+  if (!otherUser) { //Still loading
+    return null;
   }
   
   return (
     <TouchableWithoutFeedback onPress={onClick}>
       <View style={styles.container}>
       <View style={styles.leftContainer}>
-        <Image source={{ uri: user.imageUri }} style={styles.avatar} />
+      <Image source={{ uri: otherUser.imageUri }} style={styles.avatar} />
         <View style={styles.midContainer}>
-          <Text style={styles.username}>{user.name}</Text>
-          <Text numberOflines={2} style={styles.lastMessage}>{chatRoom.lastMessage.content}</Text>
+          <Text style={styles.username}>{otherUser.name}</Text>
+          <Text numberOflines={2} style={styles.lastMessage}>{chatRoom.lastMessage ? chatRoom.lastMessage.content : ''}</Text>
         </View>
       </View>
-      <Text style={styles.time}>{moment(chatRoom.lastMessage.createdAt).format('DD/MM/YYYY')}</Text>
+      {chatRoom.lastMessage && <Text style={styles.time}>{moment(chatRoom.lastMessage.createdAt).format('DD/MM/YYYY')}</Text>}
     </View>
     </TouchableWithoutFeedback>
   );
--- screens/ChatsScreen.tsx
+++ screens/ChatsScreen.tsx
@@ -1,19 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, FlatList } from 'react-native';
 import { Auth, graphqlOperation, API } from 'aws-amplify';
 import { View } from '../components/Themed';
-import chatRooms from '../data/ChatRooms'
 import ChatListItem from '../components/ChatListItem'
 import NewMessageButton from '../components/NewMessageButton';
 import { getUser } from './queries';
 
 export default function ChatsScreen() {
+  const [chatRooms, setChatRooms] = useState([]);
   useEffect(() => {
     const fetchChatRooms = async () => {
       try {
         const userInfo = await Auth.currentAuthenticatedUser({ bypassCache: true })
         const userData = await API.graphql(graphqlOperation(getUser, { id: userInfo.attributes.sub }));
         // console.log(userData);
+        setChatRooms(userData.data.getUser.chatRoomUser.items)
+
       } catch (e) {
         console.log(e);
       }
@@ -27,7 +29,7 @@ export default function ChatsScreen() {
       <FlatList
         style={{ width: '100%' }}
         data={chatRooms}
-        renderItem={({ item }) => <ChatListItem chatRoom={item} />}>
+        renderItem={({ item }) => <ChatListItem chatRoom={item.chatRoom} />}>
         keyExtractor={(item) => item.id}
       </FlatList>
       <NewMessageButton />
