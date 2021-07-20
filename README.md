4fc2df36f3dab213db1ea4770db31c5e3a2078ec
Bluette1 <marylene.sawyer@gmail.com>
2021-07-20 04:15:31 +0200

    Add the readme

---
+++ README.md
@@ -0,0 +1,50 @@
+### star-wars-info
+
+This project is a real-time chat application based on the whatsapp design, built using AWS Amplify and React-Native.
+
+![Demo picture](./assets/images/screenshot_expo.jpg)
+
+
+## Built With
+- React-Native
+- Typescript
+- GraphQL
+- Nodejs
+- DynamoDB
+- AWS Amplify
+
+## Description
+This project is a real-time chat application based on the whatsapp design, built using AWS Amplify and React-Native.
+
+
+## Run server locally
+Clone the git repo.
+
+- To start the client, open the terminal and inside the project root folder type the following 
+```
+yarn install
+yarn start
+```
+
+
+## Authors
+
+👤 **Marylene Sawyer**
+- Github: [@Bluette1](https://github.com/Bluette1)
+- Twitter: [@MaryleneSawyer](https://twitter.com/MaryleneSawyer)
+- Linkedin: [Marylene Sawyer](https://www.linkedin.com/in/marylene-sawyer-b4ba1295/)
+
+
+# Acknowledgements
+
+- The content in this repository was retrieved from or inspired by the following sites
+  - [React-Native Documentation](https://reactnative.dev/docs/getting-started)
+  - [Build a Realtime Chat App in React Native (tutorial for beginners)](https://www.youtube.com/watch?v=EvSUJ5lUcBw)
+
+## Potential Future Improvements / Ambitions
+- Review and add more display features
+- Add more tabs such as the status screen
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!
--- screens/ChatRoomScreen.tsx
+++ screens/ChatRoomScreen.tsx
@@ -1,43 +1,40 @@
 import React, { useEffect, useState } from 'react';
-import { FlatList, ImageBackground } from 'react-native';
+import { FlatList, ImageBackground, KeyboardAvoidingView, Platform } from 'react-native';
 import { useRoute } from '@react-navigation/native';
 import { Auth, API, graphqlOperation } from 'aws-amplify';
-import {messagesByChatRoom} from '../src/graphql/queries';
-import {onCreateMessage} from '../src/graphql/subscriptions';
+import { messagesByChatRoom } from '../src/graphql/queries';
+import { onCreateMessage } from '../src/graphql/subscriptions';
 
 import ChatMessage from '../components/ChatMessage';
-import {BG} from '../assets/images/BG.png';
+import { BG } from '../assets/images/BG.png';
 import InputBox from '../components/InputBox';
+import styles from '../components/ChatListItem/style';
 
 const ChatRoomScreen = () => {
   const [messages, setMessages] = useState([]);
   const [myId, setMyId] = useState(null);
   const route = useRoute();
-  const fetchMessages = async() => {
+  const fetchMessages = async () => {
     const messagesData = await API.graphql(graphqlOperation(
       messagesByChatRoom,
-      {chatRoomId: route.params.id, sortDirection: 'DESC'},
+      { chatRoomId: route.params.id, sortDirection: 'DESC' },
     ));
-    
+
     setMessages(messagesData.data.messagesByChatRoom.items);
   };
 
   useEffect(() => {
-
     fetchMessages();
-
   }, []);
 
   useEffect(() => {
 
-    const fetchUser = async() => {
-      const userInfo = await Auth.currentAuthenticatedUser({bypassCache: true});
+    const fetchUser = async () => {
+      const userInfo = await Auth.currentAuthenticatedUser({ bypassCache: true });
 
       setMyId(userInfo.attributes.sub);
     };
-
     fetchUser();
-
   }, []);
 
   useEffect(() => { //subscriptions are synchronous
@@ -46,9 +43,8 @@ const ChatRoomScreen = () => {
         graphqlOperation(onCreateMessage)
       ).subscribe({
         next: (data) => {
-          
+
           const newMessage = data.value.data.onCreateMessage;
-          console.log('newMessage: ', newMessage)
           if (newMessage.chatRoomId !== route.params.id) {
             return;
           }
@@ -56,22 +52,22 @@ const ChatRoomScreen = () => {
         }
       });
       return () => subscription.unsubscribe();
-      
+
     } catch (e) {
       console.log(e)
     }
-    
+
   }, []);
 
   return (
-    <ImageBackground style={{width: '100%', height: '100%'}} source={BG}>
-        <FlatList
-      data={messages}
-      renderItem={({ item }) => <ChatMessage message={item} myId={myId} />}
-      inverted
+    <ImageBackground style={{ width: '100%', height: '100%' }} source={BG}>
+      <FlatList
+        data={messages}
+        renderItem={({ item }) => <ChatMessage message={item} myId={myId} />}
+        inverted
       >
-    </FlatList>
-    <InputBox chatRoomId={route.params.id} />
+      </FlatList>
+      <InputBox chatRoomId={route.params.id} />
     </ImageBackground>
   )
 }
